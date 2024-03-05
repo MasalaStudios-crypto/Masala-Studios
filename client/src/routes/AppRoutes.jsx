cb403361d0c9010063fe67f917388b0a46d3d739
sergio <sergiowani@gmail.com>
2024-03-05 20:41:03 +0100

    quienes somos

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -7,6 +7,7 @@ import { TravelContext } from '../Context/TravelsProvider'
 import { NavBarApp } from '../components/NavBarApp/NavBarApp'
 import { Home } from '../pages/Home/Home'
 import { Footer } from '../components/Footer/Footer'
+import { Who } from '../pages/Who/Who'
 
 
 export const AppRoutes = () => {
@@ -24,6 +25,7 @@ export const AppRoutes = () => {
         <main>
           <Routes>
             <Route path='/' element={<Home />}/>
+            <Route path='/who' element={<Who />}/>
             <Route path='/about' element={<h1>ABOUT</h1>} />
             <Route path='/service' element={<h1>SERVICE</h1>} />
             <Route path="*" element={<h1>TRAMPOSO</h1>}/>
