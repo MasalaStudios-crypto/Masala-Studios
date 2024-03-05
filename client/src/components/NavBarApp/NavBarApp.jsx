16c7baa58f4ce86a6bda898d5425b9b79118fa71
sergio <sergiowani@gmail.com>
2024-03-05 14:00:08 +0100

    navbar

---
+++ client/src/components/NavBarApp/NavBarApp.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Button, Container, Nav, Navbar, NavDropdown  } from 'react-bootstrap'
+import './navbar.scss'
+
+export const NavBarApp= () => {
+
+
+  return (
+    <Navbar expand="lg" className="custom-navbar-bg">
+      <Container fluid>
+        <Navbar.Brand href="/"><img src="/images/logo.png" alt="logotipo" className='logo-nav' /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll" >
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">PORTFOLIO</Nav.Link>
+            <Nav.Link href="#action2">QUIENES SOMOS</Nav.Link>
+            <Nav.Link href="#action2">SERVICIOS</Nav.Link>
+            <Nav.Link href="#action2">BLOG</Nav.Link>
+            <Nav.Link href="#action2">CONTACTO</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Button>Iniciar sesión</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
